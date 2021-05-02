45395cb474d68331e82ee8685edb18b33040f948
Bekuuu <75431869+Bekuuu@users.noreply.github.com>
2021-05-02 15:17:28 +0800

    Create buf4.s

---
+++ buflab/buf4.s
@@ -0,0 +1,11 @@
+
+buf4.o：     文件格式 elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	b8 48 60 94 65       	mov    $0x65946048,%eax
+   5:	bd 50 3d 68 55       	mov    $0x55683d50,%ebp
+   a:	68 be 8d 04 08       	pushq  $0x8048dbe
+   f:	c3                   	retq   
